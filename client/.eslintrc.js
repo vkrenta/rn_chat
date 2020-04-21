a2cb0529860d488d17dec869763fef8d328c7274
vkrenta <0vkrenta@gmail.com>
2020-04-21 11:54:06 +0300

    init client file structure

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'no-var': 'warn',
+    eqeqeq: ['warn', 'smart'],
+    'no-console': 'warn',
+  },
 };
