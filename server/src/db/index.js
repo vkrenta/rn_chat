f86589b11c9cc6f5818e2098014bc3f651286c06
vkrenta <0vkrenta@gmail.com>
2020-04-22 16:05:38 +0300

    added connection to mongo

---
+++ server/src/db/index.js
@@ -0,0 +1,16 @@
+const { connect } = require('mongoose');
+const { MONGO_URI } = require('../config');
+const { log } = require('../helpers');
+
+module.exports = async () => {
+  try {
+    await connect(MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    log.info('Successful connection to MongoDB');
+  } catch (e) {
+    log.error({ label: 'MONGO', message: e });
+    process.exit(-1);
+  }
+};
