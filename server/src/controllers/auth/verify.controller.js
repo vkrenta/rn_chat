fb021d6303ca7156059763e977484b6ef56cac5c
vkrenta <0vkrenta@gmail.com>
2020-04-24 20:27:46 +0300

    added links verification

---
+++ server/src/controllers/auth/verify.controller.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const {
+  auth: { createUser, isUserExists },
+} = require('../../db/methods');
+const { resType } = require('../../helpers');
+const { LINK_SECRET } = require('../../config');
+
+module.exports = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userName, email, password, firstName, lastName } = jwt.verify(
+      id,
+      LINK_SECRET
+    );
+    const isExist = await isUserExists({ userName });
+    if (isExist)
+      return res
+        .status(200)
+        .send({ type: resType.info, message: `You cant activate twice` });
+    await createUser({ userName, email, password, firstName, lastName });
+    res
+      .status(200)
+      .send({ type: resType.info, message: `Created user ${userName}` });
+    // instead send html file
+  } catch (e) {
+    if (e.name === 'TokenExpiredError')
+      res.status(201).send({ type: resType.info, message: `Link expired` });
+    // and here send html file
+    next(e);
+  }
+};
