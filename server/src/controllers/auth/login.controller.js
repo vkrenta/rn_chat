45f0f858347df49aae0f5ab86f569db1fa746f17
ikiruswan <ikiruswan@gmail.com>
2020-04-29 16:30:11 +0300

    Misc fixes in authorization controller

--- server/src/controllers/auth/login.controller.js
+++ server/src/controllers/auth/login.controller.js
@@ -17,13 +17,13 @@ module.exports = async (req, res, next) => {
       res.status(401).send({type: 'info', payload: "Wrong username or email"});
     
     if (!bcrypt.compare(password, user.password))
-      res.status(200).send({ type: 'info', payload: "Wrong password" });
+      res.status(401).send({ type: 'info', payload: "Wrong password" });
     
     // Making sign function look more compact
     const dataToSign = { userId: user._id, userName: user.name };
     const jwToken = jwt.sign(dataToSign, process.env.AUTH_SECRET);
 
-    res.status(200).send({ type: 'data', token: jwToken });
+    res.status(200).send({ type: 'data', payload: { token: jwToken } });
   } catch (e) {
     next(e);
   }
